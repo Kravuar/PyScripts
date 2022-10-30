205df84478d2344f2b5e30bb95bb7cc10d08e258
Kravuar <Steam_Webmoney@mail.ru>
2022-10-30 14:09:19 +0300

    Kinda done
    + more args, + a bit of functionality

--- textcmp.py
+++ textcmp.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from colorama import Fore, Style
 from recordclass import dataobject
-from argparse import ArgumentParser
+from itertools import islice
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 
 class Difference(dataobject):
@@ -34,7 +35,7 @@ def file_valid(parser, file):
         parser.error(err_invalid_file.format(file))
 
 
-def find_difs(*lines):
+def find_difs(lines):  # *lines
     difs = []
     zipped = enumerate(zip(*lines))
     for idx, chars in zipped:
@@ -55,35 +56,65 @@ def find_difs(*lines):
 
 def print_styled(s, difs):
     prev = 0
-    for dif in difs:
-        print(match.format(s[prev:dif.start]), end='')
-        print(difference.format(s[dif.start:dif.end]), end='')
-        prev = dif.end
+    if (len(difs) == 0):
+        print(match.format(s), end='')
+    else:
+        for dif in difs:
+            print(match.format(s[prev:dif.start]), end='')
+            print(difference.format(s[dif.start:dif.end]), end='')
+            prev = dif.end
     print()
 
 
-def main(files):
-    for idx, lines in enumerate(zip(*files)):
-        difs = find_difs(*lines)
+def print_difs(difs, lines=None, files=None):  # Eh
+    print(difs)
+
+
+def print_lines(difs, lines, files):
+    for file, line in enumerate(lines):
+        print(infoSecondary.format('*' + files[file].name + '*'))
+        print_styled(line, difs)
+
+
+def print_full(difs, lines, files):
+    print_lines(difs, lines, files)
+    print_difs(difs)
+
+
+def main(args):
+    printer = (print_difs
+                if args.only_difs
+                else (print_full
+                        if args.difs
+                        else print_lines))
+
+    for idx, lines in islice(enumerate(zip(*args.files)), args.start, args.until):
         print(infoPrimary.format("--------------Line #{}--------------".format(idx)))
-        for file, line in enumerate(lines):
-            print(infoSecondary.format('*' + files[file].name + '*'))
-            print_styled(line, difs)
+        printer(find_difs(lines), lines, args.files)
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description='Script which finds differencies between text files.')
-    parser.add_argument("-f", dest="files", required=True,
-                        help="input files to compare", metavar="FILE",
+    parser = ArgumentParser(
+        prog='TextCMP',
+        formatter_class=RawDescriptionHelpFormatter,
+        description='''
+---------------------------------------------------------------
+    Script which finds differences between text files.
+    Comparing files line by line (hence it compares until
+    shortest file is exhausted), highlighting differences.
+---------------------------------------------------------------
+        ''')
+    parser._positionals.title = "Required"
+    parser._optionals.title = "Optional"
+    parser.add_argument("files", help="input files to compare", metavar="FILES",
                         nargs='+', type=lambda x: file_valid(parser, x))
+    parser.add_argument("-d", "--difs", action="store_true",
+                        help="output differences list")
+    parser.add_argument("-D", "--only-difs", action="store_true",
+                        help="output only differences list")
+    parser.add_argument("-s", "--start", type=int,
+                        help="from nth line")
+    parser.add_argument("-u", "--until", type=int,
+                        help="until nth line")
     args = parser.parse_args()
-    main(args.files)
-    # compare string not files
-    # compare arbitrary amount of strings/files
-    # argument -f to compare files instead of strings followed by arbitrary amount of files
-    # argument -o output difs list
-    # argument -O output only difs list
-
-    # else:
-    #     print(err_invalid_arg_amount)
-    #     parser.print_help()
+    main(args)
